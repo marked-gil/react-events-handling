ed0bfd75ffd62e9430717a3b71d7c2668fae1f45
marked-gil <markgil_creates@yahoo.com>
2023-01-29 22:50:05 +0000

    Events Handling

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+// import EventsClass from './components/EventsClass';
+// import EventsFunctional from './components/EventsFunctional';
+import EventBinding from './components/EventBinding';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <EventsClass />
+      <EventsFunctional /> */}
+      <EventBinding />
+    </div>
+  );
+}
+
+export default App;
